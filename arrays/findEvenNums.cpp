1ec1ac02437418c0e3175558644db6b0c05c183b
Nick Yoo <nick@Nicks-MacBook-Pro.local>
2022-05-23 17:16:13 +0900

    added sortedSquares.cpp

--- arrays/findEvenNums.cpp
+++ arrays/findEvenNums.cpp
@@ -4,12 +4,15 @@
 using namespace std;
 using std::vector;
 
+//function that determines the number of digits
 int countDigit(int number) {
     return int(log10(number) + 1);
 }
+//function that finds number of even number of digits
 int findEvenNums(int num[], int size) {
     int evenNumDigit = 0;
     for (int i = 0; i < size; i++) {
+        // if divisible by 2 => even
         if (countDigit(num[i]) % 2 == 0) {
             evenNumDigit++;
         }
@@ -17,12 +20,11 @@ int findEvenNums(int num[], int size) {
     return evenNumDigit;
 }
 
-
 int main() {
     int arr[5] = {12,345,2,6,7896};
 
     int res = findEvenNums(arr, 5);
     cout << res << endl;
     return 0;
-    
+
 }
---
+++ arrays/sortedSquares.cpp
@@ -0,0 +1,26 @@
+
+#include<cmath>
+#include<iostream>
+using namespace std;
+using std::vector;
+using std::sort;
+
+//function that finds number of even number of digits
+ int* sortedSquares(int num[], int size, int temp[]) {
+    for (int i = 0; i < size; i++) {
+        temp[i] = int(pow(num[i], 2));
+    }
+    return temp;
+}
+
+
+int main() {
+    int arr[5] = {-4,-1,0,3,10};
+    int tempArr[5];
+    int* newArr = sortedSquares(arr, 5, tempArr);
+    for (int i = 0; i < 5; i++) {
+        cout << newArr[i] << " ";
+    }
+    return 0;
+    
+}
